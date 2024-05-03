3f317be8b76102755b707dfe4fe560d6c466e785
Jidh <yeshusavior@gmail.com>
2024-05-03 11:00:24 +0100

    Initial Commi

---
+++ TopUsers/TopUsersApp.swift
@@ -0,0 +1,17 @@
+//
+//  TopUsersApp.swift
+//  TopUsers
+//
+//  Created by Jidh George on 03/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct TopUsersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
