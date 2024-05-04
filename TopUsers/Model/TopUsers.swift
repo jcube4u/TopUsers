f2f7683b58019a2b1e704b964049a29eaf3e7339
Jidh <yeshusavior@gmail.com>
2024-05-04 18:10:14 +0100

    Rename TopUsers to TopUsersView

--- TopUsers/Model/TopUsers.swift
+++ TopUsers/Model/TopUsers.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+
+struct TopUsers: Codable,Equatable {
+    let items:[User]
+}
+
+struct User: Codable, Equatable {
+    let userId: Int
+    let displayName: String
+    let reputation: Int
+    let profileImage: String
+    let location: String?
+}
--- TopUsers/Screens/TopUsers/TopUsersView.swift
+++ TopUsers/Screens/TopUsers/TopUsersView.swift
@@ -8,12 +8,42 @@
 import SwiftUI
 /**:
     TopUsersView Display the list of top 20 Stackoverflow Users.
+    Display Content based in View State that is managed in the ViewModel
  */
 struct TopUsersView: View {
+    @State var viewModel =  TopUsersViewModel(dataService: TopUsersDataService())
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            
+            switch viewModel.state {
+                case .notIntiated:
+                ContentUnavailableView("Data not available", image: "exclamationmark.cloud.fill")
+                case.loading:
+                    ZStack {
+                        Color.gray
+                        ProgressView()
+                    }.frame(width: 100, height: 100)
+                        .clipShape(RoundedRectangle(cornerRadius:  25.0))
+                        
+                case .noData:
+                    ContentUnavailableView("Data not available", image: "exclamationmark.cloud.fill")
+                        
+                case let .topUsers(results: topusers):
+                    List(topusers, id: \.userId) { user in
+                            TopUserRowView(user: user)
+                                .frame(height: 100)
+                        }
+                
+            }
+       
+        }
+        .onAppear(perform: {
+            Task { await viewModel.fetchUsers() }
+        })
+
     }
 }
+
 
 #Preview {
     TopUsersView()
