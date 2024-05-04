fb171b71b8fdb187b72005ad4157b45e281eb1bf
Jidh <yeshusavior@gmail.com>
2024-05-05 00:35:29 +0100

    Updated Followers View to use custome button Style

--- TopUsers/Screens/TopUsers/FollowerView.swift
+++ TopUsers/Screens/TopUsers/FollowerView.swift
@@ -10,12 +10,28 @@ import SwiftUI
 struct FollowerView: View {
     @State private var isSelected =  false
     var body: some View {
-
         let text =  !isSelected ? "Follow" :  "UnFollow"
         Button(text) {
             isSelected.toggle()
         }
-        .buttonStyle(StandardButtonStyle())
+        .buttonStyle(FollowingButtonStyle(isEnabled: isSelected))
+    }
+}
+
+struct FollowingButtonStyle: ButtonStyle {
+
+    var isEnabled: Bool
+    func makeBody(configuration: Configuration) -> some View {
+        
+        let backgroundColor = isEnabled ? Color.primarylightGreen : Color.primaryNavyBlue
+        let foregroundColor = isEnabled ? Color.black :  Color.white
+        configuration.label
+            .font(.caption2)
+            .foregroundColor(foregroundColor)
+            .padding()
+            .background(backgroundColor)
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+        
     }
 }
 
